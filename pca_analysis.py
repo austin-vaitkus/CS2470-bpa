4f3b8462e3de7fc2f49b572c214479105c0864f5
Austin Vaitkus <58187733+austin-vaitkus@users.noreply.github.com>
2019-12-11 00:04:06 -0500

    Woops minor fix

--- pca_analysis.py
+++ pca_analysis.py
@@ -192,23 +192,3 @@ def K_Nearest_Neighbor(known_points, labels, unknown_points, pulse_species, save
   
     return   
     
-    
-    
-  #%%
-
-
-    # # Plot the decision boundary. For that, we will assign a color to each
-    # # point in the mesh [x_min, x_max]x[y_min, y_max].    
-    # h = .02  # step size in the mesh
-    # x_min, x_max = known_points[:, 0].min() - 1, known_points[:, 0].max() + 1
-    # y_min, y_max = known_points[:, 1].min() - 1, known_points[:, 1].max() + 1
-    # xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-    #                      np.arange(y_min, y_max, h))
-    # Z = NNfunc.predict(np.c_[xx.ravel(), yy.ravel()])
-
-
-    # # Put the result into a color plot
-    # Z = Z.reshape(xx.shape)
-    
-    # plt.figure()
-    # plt.pcolormesh(xx, yy, Z, cmap='cmap_light')  
